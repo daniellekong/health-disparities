14319674de64601bc906360978d92004bf6975ce
daniellekong <dankon101@gmail.com>
2018-07-22 18:54:49 -0400

    first commit

---
+++ js/data-chart.js
@@ -0,0 +1,90 @@
+var ctx = document.getElementById('canvas').getContext('2d');
+
+var states = new Array();
+var diabetes = new Array();
+var income = new Array();
+
+for (var i = 0; i < data.length; i++) {
+    states[i] = data[i].state
+}; 
+
+for (var i = 0; i < data.length; i++) {
+    diabetes[i] = data[i].percent_diabetes
+};
+
+for (var i = 0; i < data.length; i++) {
+    income[i] = data[i].mean_income
+};
+
+var chartData = {
+    labels: states,
+    datasets: [{
+        type: 'line',
+        label: 'Percent of Adults with Diabetes',
+        pointBackgroundColor: '#FF533D',
+        backgroundColor: '#FF533D',
+        data: diabetes,
+        yAxisID: 'diabetes-axis',
+        showLine: false,
+        pointRadius: 5
+    }, {
+        type: 'bar',
+        label: 'Mean Income',
+        backgroundColor: '#AB987A',
+        data: income,
+        yAxisID: 'income-axis' 
+    }]
+};
+
+var option = {
+    responsive: true,
+    maintainAspectRatio: false,
+    title: {
+        display: true,
+        text: 'Interactive Chart Visualizing Diabetes Prevalence and Income by US State'
+    },
+    tooltips: {
+        mode: 'index',
+        intersect: true
+    },
+    scales:{ 
+        yAxes: [{
+            position: 'left',
+            'id': 'income-axis', 
+            ticks: {
+                min: 20000,
+                callback: function(value, index, values) {
+                        return '$' + value;
+                }
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Mean Income in US Dollars'
+            }
+        }, {
+            position: 'right',
+            'id': 'diabetes-axis',
+            ticks: {
+                min: 0,
+                max: 18,
+                callback: function(value, index, values) {
+                        return value + '%';
+                }
+            },
+            scaleLabel: {
+                display: true,
+                labelString: 'Percent of Adults with Diabetes'    
+            }    
+        }], 
+        xAxes: [{
+            gridLines: {display: false},
+            ticks: {autoSkip: false}
+        }]
+    }
+};
+
+var chart = new Chart(ctx, {
+    type: 'bar',
+    data: chartData,
+    options: option
+});
---
+++ js/diabetes-income-data.js
@@ -0,0 +1,262 @@
+var data = [
+  {
+    "state": "District of Columbia",
+    "mean_income": 107594,
+    "percent_diabetes": 9.3
+  },
+  {
+    "state": "Connecticut",
+    "mean_income": 99992,
+    "percent_diabetes": 8.1
+  },
+  {
+    "state": "New Jersey",
+    "mean_income": 99026,
+    "percent_diabetes": 7.9
+  },
+  {
+    "state": "Maryland",
+    "mean_income": 97801,
+    "percent_diabetes": 9.4
+  },
+  {
+    "state": "Massachusetts",
+    "mean_income": 94143,
+    "percent_diabetes": 8
+  },
+  {
+    "state": "Alaska",
+    "mean_income": 89746,
+    "percent_diabetes": 7.8
+  },
+  {
+    "state": "Virginia",
+    "mean_income": 88955,
+    "percent_diabetes": 9.6
+  },
+  {
+    "state": "California",
+    "mean_income": 87877,
+    "percent_diabetes": 9.6
+  },
+  {
+    "state": "Hawaii",
+    "mean_income": 87329,
+    "percent_diabetes": 7.8
+  },
+  {
+    "state": "New York",
+    "mean_income": 86825,
+    "percent_diabetes": 8.9
+  },
+  {
+    "state": "New Hampshire",
+    "mean_income": 85727,
+    "percent_diabetes": 6.8
+  },
+  {
+    "state": "Colorado",
+    "mean_income": 81635,
+    "percent_diabetes": 6.4
+  },
+  {
+    "state": "Washington",
+    "mean_income": 80789,
+    "percent_diabetes": 7.7
+  },
+  {
+    "state": "Minnesota",
+    "mean_income": 80444,
+    "percent_diabetes": 6.9
+  },
+  {
+    "state": "Illinois",
+    "mean_income": 79556,
+    "percent_diabetes": 9.1
+  },
+  {
+    "state": "Delaware",
+    "mean_income": 79262,
+    "percent_diabetes": 9.9
+  },
+  {
+    "state": "Rhode Island",
+    "mean_income": 77111,
+    "percent_diabetes": 7.9
+  },
+  {
+    "state": "Utah",
+    "mean_income": 76531,
+    "percent_diabetes": 7.5
+  },
+  {
+    "state": "North Dakota",
+    "mean_income": 76185,
+    "percent_diabetes": 8.1
+  },
+  {
+    "state": "Texas",
+    "mean_income": 75264,
+    "percent_diabetes": 11.2
+  },
+  {
+    "state": "Wyoming",
+    "mean_income": 73477,
+    "percent_diabetes": 7.6
+  },
+  {
+    "state": "Pennsylvania",
+    "mean_income": 73175,
+    "percent_diabetes": 8.8
+  },
+  {
+    "state": "Vermont",
+    "mean_income": 71392,
+    "percent_diabetes": 7.1
+  },
+  {
+    "state": "Kansas",
+    "mean_income": 70093,
+    "percent_diabetes": 8.9
+  },
+  {
+    "state": "Nebraska",
+    "mean_income": 69392,
+    "percent_diabetes": 8
+  },
+  {
+    "state": "Wisconsin",
+    "mean_income": 69333,
+    "percent_diabetes": 7.4
+  },
+  {
+    "state": "Nevada",
+    "mean_income": 69150,
+    "percent_diabetes": 9
+  },
+  {
+    "state": "Oregon",
+    "mean_income": 69040,
+    "percent_diabetes": 9.6
+  },
+  {
+    "state": "Georgia",
+    "mean_income": 69010,
+    "percent_diabetes": 10.7
+  },
+  {
+    "state": "Iowa",
+    "mean_income": 68446,
+    "percent_diabetes": 7.7
+  },
+  {
+    "state": "Arizona",
+    "mean_income": 68297,
+    "percent_diabetes": 9.1
+  },
+  {
+    "state": "Florida",
+    "mean_income": 67975,
+    "percent_diabetes": 9.3
+  },
+  {
+    "state": "Michigan",
+    "mean_income": 66760,
+    "percent_diabetes": 9.5
+  },
+  {
+    "state": "Ohio",
+    "mean_income": 66409,
+    "percent_diabetes": 9.5
+  },
+  {
+    "state": "South Dakota",
+    "mean_income": 66404,
+    "percent_diabetes": 8.4
+  },
+  {
+    "state": "North Carolina",
+    "mean_income": 65338,
+    "percent_diabetes": 9.6
+  },
+  {
+    "state": "Missouri",
+    "mean_income": 65312,
+    "percent_diabetes": 10.2
+  },
+  {
+    "state": "Maine",
+    "mean_income": 64985,
+    "percent_diabetes": 8.2
+  },
+  {
+    "state": "Indiana",
+    "mean_income": 64649,
+    "percent_diabetes": 10.5
+  },
+  {
+    "state": "Louisiana",
+    "mean_income": 63904,
+    "percent_diabetes": 11.8
+  },
+  {
+    "state": "Oklahoma",
+    "mean_income": 63890,
+    "percent_diabetes": 10.7
+  },
+  {
+    "state": "Tennessee",
+    "mean_income": 63339,
+    "percent_diabetes": 11.4
+  },
+  {
+    "state": "Montana",
+    "mean_income": 63139,
+    "percent_diabetes": 6.7
+  },
+  {
+    "state": "Idaho",
+    "mean_income": 62311,
+    "percent_diabetes": 7.3
+  },
+  {
+    "state": "New Mexico",
+    "mean_income": 62215,
+    "percent_diabetes": 10.5
+  },
+  {
+    "state": "South Carolina",
+    "mean_income": 61987,
+    "percent_diabetes": 10.5
+  },
+  {
+    "state": "Alabama",
+    "mean_income": 60511,
+    "percent_diabetes": 12
+  },
+  {
+    "state": "Kentucky",
+    "mean_income": 60031,
+    "percent_diabetes": 12.1
+  },
+  {
+    "state": "Arkansas",
+    "mean_income": 57304,
+    "percent_diabetes": 11.2
+  },
+  {
+    "state": "West Virginia",
+    "mean_income": 56425,
+    "percent_diabetes": 12.5
+  },
+  {
+    "state": "Mississippi",
+    "mean_income": 54906,
+    "percent_diabetes": 13.6
+  },
+  {
+    "state": "Puerto Rico",
+    "mean_income": 30463,
+    "percent_diabetes": 14.7
+  }
+]
